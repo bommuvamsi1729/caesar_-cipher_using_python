eac2ca1e42a95b4d31193a49abe92845e7cbe62d
Bommu vamsi <138384814+bommuvamsi1729@users.noreply.github.com>
2023-07-29 20:39:49 +0530

    Add files via upload

---
+++ caesar cipher.py
@@ -0,0 +1,43 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
+def encrypt(text,shift,direction):
+  text=list(text)
+  for i in range(len(text)):
+        if text[i] in alphabet:
+             value=alphabet.index(text[i])
+             if(direction=='decode'):
+                      shift*=-1
+             elif direction=="encode":
+                   shift=shift
+             else:
+               print("wrong format")
+               return
+             text[i]=alphabet[value+shift]
+             shift=abs(shift)
+  print(f'The {direction}d text is {"".join(text)}')
+choice="yes"
+while  choice=="yes":       
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift%=26
+  if(direction=="encode" or direction=="decode"): 
+     encrypt(text,shift,direction)
+  choice=input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+   
